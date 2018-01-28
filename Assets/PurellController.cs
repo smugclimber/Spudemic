22abcfa9e27b9308212124d2927e3cecc17ab463
Dan Colonna <danielcolonna@gmail.com>
2018-01-27 20:14:38 -0500

    update to game object

--- Assets/PurellController.cs
+++ Assets/PurellController.cs
@@ -16,10 +16,11 @@ public class PurellController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
 		GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
 	}
 
 	void onTriggerEnter2D(Collider2D other) {
-		Destroy (gameObject);
+		Destroy(gameObject);
 	}
 }

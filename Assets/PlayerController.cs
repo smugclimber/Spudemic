489e171ef00be960df124fcad9ffa87b4e412008
Dan Colonna <danielcolonna@gmail.com>
2018-01-27 19:25:37 -0500

    shooting controls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public Transform firePoint;
+	public GameObject purell;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+			Instantiate(purell, firePoint.position, firePoint.rotation);
+		}
+	}
+}

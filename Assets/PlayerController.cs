08cfe495cc153e8e7d3f3e146b05cb8b98f782ee
Dan Colonna <danielcolonna@gmail.com>
2018-01-27 20:06:33 -0500

    SHEE'S ALIIIVE

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	public float moveSpeed;
+	public float jumpHeight;
 	public Transform firePoint;
 	public GameObject purell;
 
@@ -13,7 +15,25 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		}
+
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		// fire purell
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
 			Instantiate(purell, firePoint.position, firePoint.rotation);
 		}
 	}

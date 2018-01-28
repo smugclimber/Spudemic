e9f7b6e059b7f9609c24cc070443f9e93b58583b
gwentxy <e0011065@u.nus.edu>
2018-01-27 23:11:13 -0500

    Cough Scene

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+	public float fadeRate = 4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//decrease timer
+		if(fadeRate > 0){
+			fadeRate -= Time.deltaTime;
+		} else {
+			Destroy (gameObject);
+		}
+			
+}
+}
